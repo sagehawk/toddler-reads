ead676bbce867112c2eb2a84f2f24bc2a133a095
Sajjad <haq.sajjad220@gmail.com>
2025-08-31 00:06:52 -0500

    done

--- client/src/components/login-page/index.ts
+++ client/src/components/login-page/index.ts
@@ -1,3 +1,3 @@
-export * from './login';
+export * from './components/LoginForm';
 export { FloatingLetters } from "./components/FloatingLetters";
 export { LoginForm } from "./components/LoginForm";
