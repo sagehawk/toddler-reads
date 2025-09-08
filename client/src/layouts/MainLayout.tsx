2f086dead0412b1260f159360e71d3cb5c2aa853
Sajjad <haq.sajjad220@gmail.com>
2025-09-07 23:42:42 -0500

    Deleting old landing page

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,14 +1,10 @@
-import { Header } from "@/components/landing/Header";
-import { Footer } from "@/components/landing/Footer";
-
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <Header />
       <main>
         {children}
       </main>
-      <Footer />
     </div>
   );
 }
+
